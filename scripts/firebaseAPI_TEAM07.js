75bc41dacf5fe78cd360e7e7c6126c982b3beef4
Dartleader <2326239694@qq.com>
2025-03-05 05:52:07 +0800

    Add new api file

---
+++ scripts/firebaseAPI_TEAM07.js
@@ -0,0 +1,7 @@
+apiKey: "AIzaSyAgT0zV9vzNB4x3ALTsamlYort2HSOZTBY",
+authDomain: "comp1800-demo-use-62834.firebaseapp.com",
+projectId: "comp1800-demo-use-62834",
+storageBucket: "comp1800-demo-use-62834.firebasestorage.app",
+messagingSenderId: "586911638993",
+appId: "1:586911638993:web:df7491e6d6480958faecf0",
+measurementId: "G-EGE64412ZQ"
